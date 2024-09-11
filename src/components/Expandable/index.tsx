2f2b79666cd2387734b5fc659b0b981afe100e1f
Iain Davis <iain@iaindavis.dev>
2024-09-11 11:18:29 +1000

    add Expandable component

---
+++ src/components/Expandable/index.tsx
@@ -0,0 +1,34 @@
+import React, { PropsWithChildren,  useState } from 'react';
+import styles from './expandable.module.css';
+
+export type ExpandablePropsType = {
+    startExpanded?: boolean;
+    expandPrompt?: string;
+    collapsePrompt?: string;
+}
+
+const Expandable: React.FC<PropsWithChildren<ExpandablePropsType>> = ({
+    startExpanded = false,
+    expandPrompt = 'show more...',
+    collapsePrompt = 'show less',
+    children
+}) => {
+    const [isExpanded, setExpanded] = useState(startExpanded);
+
+    const handleClickToggle = () => {
+        setExpanded(!isExpanded);
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={ `${styles.expandableBlock} ${isExpanded ? '' : styles.hidden}` }>
+                {children}
+            </div>
+            <div className={ styles.toggle } onClick={() => handleClickToggle()}>
+                {isExpanded ? collapsePrompt : expandPrompt }
+            </div>
+        </div>
+    )
+}
+
+export default Expandable;
