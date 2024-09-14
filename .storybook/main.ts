61a58102a3820e97edf39472455bb11c379e1679
Iain Davis <iain@iaindavis.dev>
2024-09-15 09:14:21 +1000

    .storybook support stuff

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
+import webpackFinal from '../src/buildUtils/webpackFinal';
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: [
-    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-themes",
@@ -18,5 +17,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
+  webpackFinal
 };
 export default config;
