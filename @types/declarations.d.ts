9818ae6e4f2e2e50def9378255d4da1df23389f3
Iain Davis <iain@iaindavis.dev>
2024-09-20 18:15:30 +1000

    shut up TypeScript

--- @types/declarations.d.ts
+++ @types/declarations.d.ts
@@ -2,3 +2,8 @@ declare module '*.mdx' {
     let MDXComponent: (props: any) => JSX.Element;
     export default MDXComponent;
 }
+
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
