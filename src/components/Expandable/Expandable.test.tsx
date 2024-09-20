c54c003ac265bee97c1cbea8e672a7ee4db3b7ae
Iain Davis <iain@iaindavis.dev>
2024-09-20 10:27:09 +1000

    add simplest-possible significant unit tests

--- src/components/Expandable/Expandable.test.tsx
+++ src/components/Expandable/Expandable.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
+import { composeStories } from '@storybook/react';
+
+import * as stories from './Expandable.stories';
+
 import Expandable from './Expandable';
+
+const { Default } = composeStories(stories);
 
 test('renders the component', () => {
     render(<Expandable />);
     expect(screen.getByText('show more...')).toBeInTheDocument();
 });
+
+test('renders the Default story correctly', () => {
+    render(
+        <Default />
+    );
+    expect(screen.getByText('show more...')).toBeInTheDocument();
+})
