ee4d341e691f5d7983d46b579a8afcbd748749e0
Iain Davis <iain@iaindavis.dev>
2024-09-22 22:05:38 +1000

    don't need the CSS modules declaration now that typescript is working more smoothly

--- @types/declarations.d.ts
+++ @types/declarations.d.ts
@@ -2,8 +2,3 @@ declare module '*.mdx' {
     let MDXComponent: (props: any) => JSX.Element;
     export default MDXComponent;
 }
-
-declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
-}
