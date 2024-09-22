e33faba9b921890095dc4ce3406f4d6aa27daf84
Iain Davis <iain@iaindavis.dev>
2024-09-22 15:48:16 +1000

    vitest configs for test reports

---
+++ vitest.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig, UserConfig, UserConfigExport, UserConfigFn } from 'vitest/config';
+import react from '@vitejs/plugin-react'
+
+const config = defineConfig(async (): Promise<UserConfig> => {
+    const {default: mdx} = await import('@mdx-js/rollup');
+
+    return ({
+        plugins: [
+            react(),
+            mdx(),
+        ],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './vitest.setup.ts',
+            include: ['src/**/*.{test,spec}.{ts,tsx,js,jsx}'],
+            reporters: [
+                'default',
+                [ 'junit', { outputFile: 'static/reports/test-results.xml', } ]
+            ],
+            coverage: {
+                provider: 'istanbul',
+                reporter: ['html', 'text', 'lcov'],
+                reportsDirectory: 'static/reports/coverage',
+                all: true,
+                exclude: [
+                    "build", 
+                    "static",
+                    ".*",   // no dot-files
+
+                    // typescript and javascript files in the root directory tend to be config files
+                    "*.ts",
+                    "*.js", 
+
+                    // I think I want to avoid testing all the markdown files. May revisit later
+                    // May just do snapshot tests on the rendered content later so at least I can detect if something breaks unexpectedly.
+                    "*.mdx",
+                    "*.md",
+                    "**/*.test.*",
+                    "**/*.spec.*",
+                    "**/*.stories.*"
+                ]
+            }
+        }
+    })
+})
+
+export default config;
